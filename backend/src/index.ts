f500101c2f39529d9d79a4587f5b0aac94c8bbcc
Weenit <weenit@synctuition.com>
2025-10-18 23:58:28 +0300

    api: fix cors issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
+import cors from "cors";
 import express from "express";
 import config from "./config";
 import { errorHandler } from "./middleware/error.middleware";
 import noteRoutes from "./routes/note.routes";
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api/notes", noteRoutes);
 
